a53e51c5491fc5325ff9dd9b6607543a0eb56fd1
Hilla Barkal <hilla.guz@gmail.com>
2017-07-09 20:40:38 +0300

    MEHATHALA

--- HomeWork7/src/HomeWork7/KMeans.java
+++ HomeWork7/src/HomeWork7/KMeans.java
@@ -2,23 +2,17 @@ package HomeWork7;
 
 import java.util.Random;
 
-import weka.clusterers.Clusterer;
 import weka.core.Instance;
 import weka.core.Instances;
 
 public class KMeans {
 	Instances centroids;
 	int k;
+	int[] clusters;
 
 	public KMeans(int k) {
 		this.k = k;
 	}
-
-//	public KMeans(Instances trainingData, Instances centroids, int k) {
-//		this.k = k;
-//		this.trainingData = trainingData;
-//		this.centroids = centroids;
-//	}
 
 	/**
 	 * This method is building the KMeans object. It should initialize centroids
@@ -38,48 +32,14 @@ public class KMeans {
 	 * 
 	 * @param instances
 	 */
-	public void initializeCentroids(Instances instances) {
-		Instances randomInstances = new Instances(instances);
-		randomInstances.randomize(new Random());
+	private void initializeCentroids(Instances instances) {
+		Instances randInstances = new Instances(instances);
+		randInstances.randomize(new Random());
 		this.centroids = new Instances(instances, this.k);
-		this.centroids.clear();
-
+		
 		for (int i = 0; i < this.k; i++) {
-			this.centroids.add(randomInstances.instance(i));
+			this.centroids.add(randInstances.instance(i));
 		}
-	}
-	/**
-	 * the function returns an array size of all instances,
-	 * indicating for every instances to what
-	 * cluster in belongs to (according to centroids)
-	 * @param instances
-	 * @param indexOFCentroid
-	 * @return
-	 */
-	public int[] findCluster(Instances instances){
-		
-		int numOfInstances = instances.numInstances();
-		int numOfClusters = centroids.numInstances();
-		int[] indexesOfCentroids = new int[numOfInstances];
-		double minDistance = Double.MAX_VALUE;
-		
-		// goes through all instances, checks the distances from all centroids
-		// and chooses the closest centroid of each instance.
-		for (int i = 0; i < numOfInstances; i++) {
-			indexesOfCentroids[i] = findClosestCentroid(instances.instance(i));
-//			for (int j = 0; j < numOfClusters; j++){
-//				double tempDistance = calcSquaredDistanceFromCentroid(instances.get(i),
-//						centroids.get(j));
-//				if (tempDistance < minDistance){
-//					minDistance = tempDistance;
-//					indexesOfCentroids[i] = j;
-//				}
-//			}
-//			// before starting a new instance, the distance is maximal
-//			minDistance = Double.MAX_VALUE;
-		}
-		
-		return indexesOfCentroids;
 	}
 
 	/**
@@ -93,62 +53,84 @@ public class KMeans {
 	 * 
 	 * @param instances
 	 */
-	public void findKMeansCentroids(Instances instances) {
-		int numOfIterations = 40;
-		double epsilon = 0.003;
-		double prevError = calcAvgWSSSE(instances);
-		double curError;
-		boolean epsilonError = false;
-		
-		for (int i = 0; i < numOfIterations && !epsilonError; i++) {
-			for (int j = 0; j < centroids.numInstances(); j++) {
-				updateIthCentroid(instances, findCluster(instances), j);
+	private void findKMeansCentroids(Instances instances) {
+		boolean costFuncChanged = false;
+		double prevError = Integer.MAX_VALUE;
+		double EPSILON = 0.003;
+		for (int i = 0; i < 40 && !costFuncChanged; i++) {
+			// cluster the points into k clusters.
+			assignClusters(instances);
+			
+			// Recalculate the new cluster center
+			reSetCentroids(instances);
+			
+			// calculate the new error and stop if the cost 
+			// function did not change much
+			double newError = calcAvgWSSSE(instances);
+			if (Math.abs(prevError - newError) < EPSILON) {
+				costFuncChanged = true;
 			}
-			System.out.println(i);
-			curError = calcAvgWSSSE(instances);
-			double diff = Math.abs(prevError - curError);
-			if (diff < epsilon) {
-				epsilonError = true;
-			}
-			prevError = curError;				
+			prevError = newError;
 		}
+	}
+
+	/**
+	 * for every instances to what
+	 * cluster in belongs to (according to centroids)
+	 * @param instances
+	 */
+	private void assignClusters(Instances instances){
+		int[] clusters = new int[instances.numInstances()];
+		for (int i = 0; i < instances.numInstances(); i++) {
+			Instance curInstance = instances.instance(i);
+			int closestCentroid = findClosestCentroid(curInstance);
+			clusters[i] = closestCentroid;
+		}
+		this.clusters = clusters;
 	}
 	
 	/**
 	 * with the given centroid, checks all instances in that cluster
 	 * and updates the centroid to be the center of the cluster
 	 * @param instances
-	 * @param indexesOfCentroids
-	 * @param indexOfCentroid
 	 */
-	public void updateIthCentroid(Instances instances, int[] indexesOfCentroids,
-									int indexOfCentroid){
-		
-		Instances cluster = new Instances(instances, instances.size());
-		cluster.clear();
-		double[] sumOfAttributes = new double[instances.numAttributes()];
-		
-		// adds all instances that belong to the cluster to the cluster itself
-		for (int i = 0; i < instances.numInstances(); i++) {
-			if (indexesOfCentroids[i] == indexOfCentroid){
-				cluster.add(instances.instance(i));
+	private void reSetCentroids(Instances instances) {
+		for (int centroid = 0; centroid < this.k; centroid++) {
+			// calculate centroids new values
+			double[] avgAttValues = calcCentroidValues(instances, centroid);
+			Instance curCentroid = this.centroids.instance(centroid);
+			// set centroids new values
+			for (int i = 0; i < avgAttValues.length; i++) {
+				curCentroid.setValue(curCentroid.attribute(i), avgAttValues[i]);
 			}
 		}
-		
-		// sums all values for each attribute of the instances in 
-		// the cluster in order to find the mean value of every attribute (RGB...)
-		for (int i = 0; i < cluster.numInstances(); i++) {
-			for (int j = 0; j < cluster.numAttributes(); j++) {
-				sumOfAttributes[j] += cluster.instance(i).value(j);
+	}
+	
+	/**
+	 * @param instances
+	 * @param clusters
+	 * @param centroid
+	 */
+	private double[] calcCentroidValues(Instances instances, int centroid) {
+		int numInstInCluster = 0;
+		for (int i = 0; i < clusters.length; i++) {
+			if (clusters[i] == centroid) {
+				numInstInCluster++;
 			}
 		}
-		
-		// calculates the mean value of the soon to be centroid
-		// sets those values to be the new centroids
-		for (int i = 0; i < sumOfAttributes.length; i++) {
-			sumOfAttributes[i] = sumOfAttributes[i] / (double) cluster.numInstances();
-			centroids.instance(indexOfCentroid).setValue(cluster.attribute(i), sumOfAttributes[i]);
+		double[] sumAttributes = new double[instances.numAttributes()];
+		for (int inst = 0; inst < instances.numInstances(); inst++) {
+			if (clusters[inst] == centroid) {
+				for (int att = 0; att < sumAttributes.length; att++) {
+					sumAttributes[att] += instances.instance(inst).value(att);
+				}
+			}
 		}
+		double[] avgAttValues = new double[instances.numAttributes()];
+		for (int att = 0; att < sumAttributes.length; att++) {
+			avgAttValues[att] = sumAttributes[att] / (double) numInstInCluster;
+		}
+		return avgAttValues;
 	}
 
 	/**
@@ -158,14 +140,13 @@ public class KMeans {
 	 * @param centroid - a centroid 
 	 * @return distance between 2 instances
 	 */
-	public double calcSquaredDistanceFromCentroid(Instance dataSetInstance, 
+	private double calcSquaredDistanceFromCentroid(Instance dataSetInstance, 
 													Instance centroid) {
-		double distance = 0;
+		double sqDistance = 0;
 		for (int i = 0; i < dataSetInstance.numAttributes(); i++) {
-			double tempDist = dataSetInstance.value(i) - centroid.value(i);
-			distance += Math.pow(tempDist, 2);
+			sqDistance += Math.pow((dataSetInstance.value(i) - centroid.value(i)), 2);
 		}
-		return distance;
+		return sqDistance;
 	}
 
 	/**
@@ -174,17 +155,17 @@ public class KMeans {
 	 * @param instance
 	 * @return index of closest centroid
 	 */
-	public int findClosestCentroid(Instance instance) {
-		int closestCentroidIndex = 0;
+	private int findClosestCentroid(Instance instance) {
 		double closestDistance = calcSquaredDistanceFromCentroid(instance, this.centroids.instance(0));
+		int closestCentroid = 0;
 		for (int i = 1; i < this.centroids.numInstances(); i++) {
 			double curDistance = calcSquaredDistanceFromCentroid(instance, this.centroids.instance(i));
-			if (closestDistance > curDistance) {
+			if (curDistance < closestDistance) {
 				closestDistance = curDistance;
-				closestCentroidIndex = i;
+				closestCentroid = i;
 			}
 		}
-		return closestCentroidIndex;
+		return closestCentroid;
 	}
 
 	/**
@@ -194,13 +175,12 @@ public class KMeans {
 	 * @return new set of closest centroids Instances 
 	 */
 	public Instances quantize(Instances instances) {
-		Instances closestCentroids = new Instances(instances, instances.numInstances());
-		closestCentroids.clear();
-		for (Instance instance : instances) {
-			int closestCentroidIndex = findClosestCentroid(instance);
-			closestCentroids.add(this.centroids.instance(closestCentroidIndex));
+		Instances replacedInst = new Instances (instances, instances.numInstances());
+		for (int i = 0; i < instances.numInstances(); i++) {
+			int centroid = this.clusters[i];
+			replacedInst.add(this.centroids.instance(centroid));
 		}
-		return closestCentroids;
+		return replacedInst;
 	}
 
 	/**
@@ -211,22 +191,14 @@ public class KMeans {
 	 * @param instances
 	 * @return double value of the WSSSE. 
 	 */
-	public double calcAvgWSSSE(Instances instances) {
-		int[] clusters = findCluster(instances);
+	private double calcAvgWSSSE(Instances instances) {
 		double Tr = 0;
-		for (int i = 0; i < clusters.length; i++) {
+		for (int i = 0; i < this.clusters.length; i++) {
 			Instance instance = instances.instance(i);
-			Instance centroid = centroids.instance(clusters[i]);
+			Instance centroid = this.centroids.instance(clusters[i]);
 			double squaredDistance = calcSquaredDistanceFromCentroid(instance, centroid);
 			Tr += squaredDistance;
 		}
-//		for (Instance instance : instances) {
-//			int closestCentroidIndex = findClosestCentroid(instance);
-//			Instance closestCentroid = this.centroids.instance(closestCentroidIndex);
-//			double squaredDistance = calcSquaredDistanceFromCentroid(instance, closestCentroid);
-//			Tr += squaredDistance;
-//		}
-		
 		return Tr / (double) instances.numInstances();
 	}
 }
